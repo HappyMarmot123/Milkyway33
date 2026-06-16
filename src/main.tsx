6b7041db0ecd115282e70671f001c440ea8c9f3f
Lucas <mdnsw28@gmail.com>
2026-06-17 08:52:52 +0900

    feat: react-scan

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { scan } from 'react-scan'
 import './index.css'
 import App from './App.jsx'
 import { ThemeProvider } from './contexts/ThemeContext'
+
+scan({
+  enabled: import.meta.env.DEV,
+  showToolbar: true,
+})
 
 const root = document.getElementById('root');
 
